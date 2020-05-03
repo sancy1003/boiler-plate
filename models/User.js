657c5d4b2f58ef7f5718cd2b52c80005f94f774d
chanyoung <kipa1003@naver.com>
2020-05-03 21:17:25 +0900

    처음 저장소에 올림

---
+++ models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        trim: true, // 문자들 사이 공백을 없어주는 기능
+        unique: 1
+    },
+    password: {
+        type: String,
+        maxlength: 5
+    },
+    role: {
+        type: Number,
+        default: 0
+    },
+    image: String,
+    token: {
+        type: String
+    },
+    tokenExp: {
+        type: Number
+    }
+})
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
